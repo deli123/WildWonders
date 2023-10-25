3d636aaee9ffe0ad30213a8dc48794516c9aa31b
Derek <liderek63@gmail.com>
2023-10-24 20:39:20 -0700

    add modal for each mountain

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -5,9 +5,9 @@ const mountainsArray = Util.sortByKey(mountainsData, "name");
 
 const mountainsContainer = document.getElementById("mountains-container");
 
-const createMountainCard = (mountain) => {
+const createMountainCard = (mountain, index) => {
   const card = `
-  <div class="card mountain col-3 mx-auto my-3" style="width: 18rem;">
+  <div class="card col-3 mx-auto my-3" id="mountain-${index}" data-bs-toggle="modal" data-bs-target="#mountain-${index}-modal" style="width: 18rem;">
     <img src="images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
     <div class="card-body">
       <h5 class="card-title text-center">${mountain.name}</h5>
@@ -16,13 +16,16 @@ const createMountainCard = (mountain) => {
   return card;
 };
 
-const createMountainModal = (mountain) => {
+const createMountainModal = (mountain, index) => {
+  // since mountain names can have two words separated by a space (eg. Mt. Washington), join them with a hypen
+  // this ensures that the id for each mountain is a single word
+  const mountainLabel = mountain.name.split(" ").join("-");
   const modal = `
-  <div class="modal fade" tabindex="-1" aria-hidden="true">
+  <div class="modal fade" id="mountain-${index}-modal" tabindex="-1" aria-labelledby="${mountainLabel}" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5">${mountain.name}</h1>
+          <h1 class="modal-title fs-5" id="${mountainLabel}">${mountain.name}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -38,19 +41,15 @@ const createMountainModal = (mountain) => {
   return modal;
 };
 
+// create a card and modal for each mountain
 const populateMountains = () => {
-  mountainsArray.forEach((mountain) => {
-    // insert each mountain after one another
+  mountainsArray.forEach((mountain, index) => {
+    // 'beforeend' inserts each mountain one after another
     mountainsContainer.insertAdjacentHTML(
       "beforeend",
-      createMountainCard(mountain)
+      createMountainCard(mountain, index) + createMountainModal(mountain, index)
     );
   });
-
-  const mountains = Array.from(document.querySelectorAll('.mountain'));
-  mountains.forEach((mountain) => {
-
-  })
 };
 
 populateMountains();
