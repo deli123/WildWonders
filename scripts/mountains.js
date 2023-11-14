a3453db067c7f0beaa578c8074e1df5cbd471039
Derek <liderek63@gmail.com>
2023-11-13 17:28:25 -0800

    add scroll to top button

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -7,8 +7,8 @@ const mountainsContainer = document.getElementById("mountains-container");
 
 const createMountainCard = (mountain, index) => {
   const card = `
-  <div class="card col-3 mx-auto my-3 mountain-card" id="mountain-${index}" data-bs-toggle="modal" data-bs-target="#mountain-${index}-modal" style="width: 18rem;">
-    <img src="images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
+  <div class="card col-3 mx-auto my-3 card-mountain" id="mountain-${index}" data-bs-toggle="modal" data-bs-target="#mountain-${index}-modal" style="width: 18rem;">
+    <img src="assets/images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
     <div class="card-body">
       <h5 class="card-title text-center">${mountain.name}</h5>
     </div>
@@ -25,7 +25,7 @@ const createMountainModal = (mountain, index) => {
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header d-flex flex-column justify-content-center">
-          <img src="images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
+          <img src="assets/images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
           <h1 class="modal-title fs-5" id="${mountainLabel}">${mountain.name}</h1>
         </div>
         <div class="modal-body text-center">
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,8 +1,28 @@
-export const sortByKey = (array, key) => {
-  // sort by alphabetical order, or least to greatest
-  return array.sort((a, b) => {
-      let x = a[key]; 
-      let y = b[key];
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-  });
+// Scroll to top button and event listeners
+const btnBackToTop = document.getElementById("btn-back-to-top");
+window.addEventListener("scroll", scrollFunction);
+btnBackToTop.addEventListener("click", backToTop);
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnBackToTop.style.display = "block";
+  } else {
+    btnBackToTop.style.display = "none";
+  }
 }
+
+// Scroll to the top of the document
+function backToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+export const sortByKey = (array, key) => {
+  // sort by alphabetical/ascending order
+  return array.sort((a, b) => {
+    let x = a[key];
+    let y = b[key];
+    return x < y ? -1 : x > y ? 1 : 0;
+  });
+};
