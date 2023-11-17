c6a45cd66ef1fceca11e415b1db1a0a09daa3403
Derek <liderek63@gmail.com>
2023-11-17 00:22:18 -0800

    create readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# WildWonders
+
+[WildWonders](https://deli123.github.io/WildWonders/) is a
+
+# Features
+
+- **Five pages**: Landing, parks, and mountains pages
+- **Responsive Design**: Accessible on all devices, ensuring a smooth experience whether you're on a desktop, tablet, or mobile phone.
+- **Search for parks**: Search through all 359 parks, possible options will be filtered on each input
+
+# Technologies
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap 5
+
+# Previews
+
+## Landing page
+
+![Landing Page]()
+
+## Parks page
+
+![Parks Page]()
+
+## Mountains page
+
+![Mountains Page]()
+
+# Code Snippet
+
--- scripts/mountains.js
+++ scripts/mountains.js
@@ -27,7 +27,7 @@ const createMountainModal = (mountain, index) => {
     <div class="modal-dialog modal-dialog-centered">
       <div class="modal-content border-3 border-light">
         <div class="modal-header d-flex flex-column justify-content-center">
-          <img src="../assets/images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
+          <img src="assets/images/mountains/${mountain.img}" class="card-img-top" alt="${mountain.name}">
           <h1 class="modal-title fs-5 mt-3" id="${mountainLabel}">${mountain.name}</h1>
         </div>
         <div class="modal-body text-center" id="mountain-${index}-body">
